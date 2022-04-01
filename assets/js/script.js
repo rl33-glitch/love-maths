ee8d54f2319470077412ca0e237f7518e9e07116
rl33-glitch <rosslowe30@gmail.com>
2022-04-01 12:36:19 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,16 @@
                  alert("You clicked Submit");
              } else {
                  let gametype = this.getAttribute("data-type");
-                 alert("You clicked ${gameType}");                             
+                runGame(gametype);                           
                 
                 }
         })
     }
 })
+
+runGame("addition");
+
+
 
 /**
  * The main game "loop" , called when the script is first loaded
@@ -24,11 +28,23 @@
 
 
 
-function runGame(){
+function runGame(gameType) {
+
     //creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if ( gameType === "addition") {
+        displayAdditionQuestion(num1,num2);
+    } else {
+        alert("Unknown game type: $ {gameType}");
+        throw "Unknown game Type: $ {gameType}. Aborting!";
+    }
 }
+
+
+
+
 
 function checkAnswer(){
 
@@ -46,8 +62,10 @@ function incremenWrongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion(){
